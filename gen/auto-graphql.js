718824a97bb7ffe6b0a0adbfd73d02ccca491646
ramblehead <v.rybynok@gmail.com>
2019-02-25 00:04:02 +0000

    Checkpoint

--- gen/auto-graphql.js
+++ gen/auto-graphql.js
@@ -1,6 +1,15 @@
 #!/usr/bin/env node
 // Hey Emacs, this is -*- coding: utf-8 -*-
 
+// https://graphqlmastery.com/blog/generate-javascript-static-types-from-graphql-typescript-and-flow
+// https://github.com/dotansimha/graphql-code-generator
+// https://github.com/prisma/graphqlgen
+// https://github.com/dangcuuson/graphql-schema-typescript
+// https://github.com/19majkel94/type-graphql
+
+
+// https://github.com/typeorm/typeorm
+
 const { generateTypesForDir } = require('./gqlt');
 
 console.log('');
